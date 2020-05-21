0cde2c98af41de4bc122bb49a348bcd5aa58f872
Orange Pi <OrangePi@root.com>
2020-05-21 16:44:30 +0800

    set sensitive

--- xiaoChuan.py
+++ xiaoChuan.py
@@ -89,7 +89,7 @@ model = 'resources/nihao2.pmdl'
 # capture SIGINT signal, e.g., Ctrl+C
 signal.signal(signal.SIGINT, signal_handler)
 
-detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
+detector = snowboydecoder.HotwordDetector(model, sensitivity=0.35)
 print ("Listening... Press Ctrl+C/Z to exit")
 
 # main loop
